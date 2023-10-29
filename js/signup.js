83b377f07c8527a04d0aeb74b248f461f43838d1
agussdangelo <agustinagustindangelo@hotmail.com>
2023-10-29 18:55:38 -0300

    se agrego elementos js de la vista inicio y registro

---
+++ js/signup.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Registro de usuario
+    const form = document.querySelector('.form');
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const passwordRepeatInput = document.getElementById('password-repeat');
+    const emailInput =  document.getElementById('email');
+    const birthDateInput =  document.getElementById('birth-date');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Evita que se envíe el formulario
+
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+        const passwordRepeat = passwordRepeatInput.value;
+
+        if (username === '' || password === '' || passwordRepeat ==='' ){
+        document.getElementById('errorDialogEmpty').showModal();
+        return false;
+         }
+            if (password === passwordRepeat) {
+            // Realizar la transformación de la contraseña
+            const transformedPassword = password.slice(password.length / 2) + password.slice(0, password.length / 2);
+
+            // Guardar los datos del usuario en el localStorage
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', transformedPassword);
+
+            // Redirigir a la página de inicio de sesión
+            window.location.href = '../index.html';
+
+            // Limpiar el formulario
+            form.reset();
+            
+        } else {
+            // Mostrar un mensaje de error si las contraseñas no coinciden
+            document.getElementById('errorDialogPasswordNotMatch').showModal();
+        }
+        
+    });
+
+});
+
+// Función para cerrar el diálogo de error por campos vacíos
+function cerrarDialogoErrorEmpty() {
+    document.getElementById('errorDialogEmpty').close();
+}
+
+// Función para cerrar el diálogo de error por contraseña desiguales
+function cerrarDialogoErrorIncorrect() {
+    document.getElementById('errorDialogPasswordNotMatch').close();
+}
