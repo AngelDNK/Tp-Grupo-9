69b874cf3f4d9d5d4f4ebe0d187b0b294805d2bc
Juan Ignacio Ronquillo <ronquillojuanignacio@gmail.com>
2023-11-02 19:23:21 -0300

    Implementación de registro, inicio de sesion y home

--- js/signup.js
+++ js/signup.js
@@ -1,34 +1,60 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Registro de usuario
-    const form = document.querySelector('.form');
-    const usernameInput = document.getElementById('username');
-    const passwordInput = document.getElementById('password');
-    const passwordRepeatInput = document.getElementById('password-repeat');
+let users = JSON.parse(localStorage.getItem('users')) || [];
 
-    form.addEventListener('submit', function (event) {
-        event.preventDefault(); // Evita que se envíe el formulario
-
-        const username = usernameInput.value;
-        const password = passwordInput.value;
-        const passwordRepeat = passwordRepeatInput.value;
-
-        if (password === passwordRepeat) {
-            // Realizar la transformación de la contraseña
-            const transformedPassword = password.slice(password.length / 2) + password.slice(0, password.length / 2);
-
-            // Guardar los datos del usuario en el localStorage
-            localStorage.setItem('username', username);
-            localStorage.setItem('password', transformedPassword);
-
-            // Redirigir a la página de inicio de sesión
-            window.location.href = '../index.html';
-
-            // Limpiar el formulario
-            form.reset();
-            
-        } else {
-            // Mostrar un mensaje de error si las contraseñas no coinciden
-            alert('Las contraseñas no coinciden. Por favor, inténtalo de nuevo.');
+// Función para registrar un usuario con validación de contraseñas coincidentes
+function registerUser(username, password, confirmPassword, email, birthdate, premium) {
+        // Verifica si el usuario o el email ya existen
+        const userExists = users.some((user) => user.username === username || user.email === email);
+        if (userExists) {
+            alert('El nombre de usuario o el email ya existen. Por favor, elija otro.');
+            return;
         }
-    });
-});
+    
+        // Verifica si las contraseñas coinciden
+        if (password !== confirmPassword) {
+            document.getElementById('errorDialogPasswordNotMatch').showModal();
+            return;
+        }
+    
+        const transformedPassword = password.slice(Math.ceil(password.length / 2)) + password.slice(0, Math.ceil(password.length / 2));
+
+        // Crea el nuevo usuario
+        const newUser = {
+            username,
+            password: transformedPassword,
+            email,
+            birthdate,
+            albumsFavoritos: [],
+            premium,
+        };
+    
+        // Agrega el usuario al array de usuarios
+        users.push(newUser);
+    
+        // Almacena el array de usuarios actualizado en el localStorage
+        localStorage.setItem('users', JSON.stringify(users));
+    
+    }    
+    // Función para manejar el envío del formulario de registro
+    function handleRegistration(event) {
+        event.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('password-repeat').value;
+        const email = document.getElementById('email').value;
+        const birthdate = document.getElementById('birth-date').value;
+        const premium = false;
+    
+        registerUser(username, password, confirmPassword, email, birthdate, premium);
+    
+        // Restablece el formulario
+        event.target.reset();
+    }
+
+    function cerrarDialogoErrorIncorrect() {
+        document.getElementById('errorDialogPasswordNotMatch').close();
+    }
+    
+    // Agrega un controlador de eventos al formulario de registro
+    document.querySelector('.form').addEventListener('submit', handleRegistration);
+
+
