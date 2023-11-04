9a8874f4d9b1ed02d688ff0bd3758c9984605a97
Angel Leyes <angelleyesdk@gmail.com>
2023-11-03 22:46:09 -0300

    se agrego el buscador
    se agrego el buscador, FALTA ARREGLAR LA GRILLA DE LOS ALBUMES

--- js/search.js
+++ js/search.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function () {
+/*document.addEventListener('DOMContentLoaded', function () {
     const search__Input = document.getElementById('search');
     const songs__section = document.querySelector('.songs__section');
 
@@ -35,4 +35,68 @@ document.addEventListener('DOMContentLoaded', function () {
 
         });
     });
+});*/
+
+
+const ALBUM1 = { album: document.querySelector('#cancerbero') }
+const ALBUM2 = { album: document.querySelector('#salandolasheridas') }
+const ALBUM3 = { album: document.querySelector('#ciro') }
+const ALBUM4 = { album: document.querySelector('#wos') }
+const ALBUM5 = { album: document.querySelector('#tupac') }
+const ALBUM6 = { album: document.querySelector('#idontwanttomissathing') }
+const ALBUM7 = { album: document.querySelector('#columbarecords') }
+const ALBUM8 = { album: document.querySelector('#kirby') }
+const ALBUM9 = { album: document.querySelector('#roadsong') }
+const ALBUM10 = { album: document.querySelector('#abba') }
+const ALBUM12 = { album: document.querySelector('#random') }
+const ALBUM13 = { album: document.querySelector('#disc') }
+const ALBUM14 = { album: document.querySelector('#piromania') }
+const ALBUM15 = { album: document.querySelector('#harrystyles') }
+const ALBUM16 = { album: document.querySelector('#somosmagic') }
+
+const ALBUMES = [ALBUM1, ALBUM2, ALBUM3, ALBUM4, ALBUM5, ALBUM6, ALBUM7, ALBUM8, ALBUM9, ALBUM10, ALBUM12, ALBUM13, ALBUM14, ALBUM15, ALBUM16];
+
+const NODO_ALBUM_BUSCAR = document.querySelector('#search');
+
+NODO_ALBUM_BUSCAR.addEventListener("keyup", e => {
+
+    const PALABRA_A_BUSCAR = NODO_ALBUM_BUSCAR.value;
+    
+    const NODO_SECTION = document.getElementById("songs_section");
+    
+    NODO_SECTION.innerHTML = "";
+
+    ALBUMES.forEach((album) => {
+        if (album && album.album) {
+            const albumNombre = album.album.id.toLowerCase();
+            
+            if (albumNombre.includes(PALABRA_A_BUSCAR)) {
+                NODO_SECTION.innerHTML += `<article><img src="${album.album.src}" alt="${album.album.alt}"></article>`;
+            }
+        }
+    });
 });
+
+    
+
+
+    /*`<article> ${ALBUMES[0].album} </article>`
+                             `<article> ${ALBUMES[1].album} </article>`
+                             `<article> ${ALBUMES[2].album} </article>`
+                             `<article> ${ALBUMES[3].album} </article>`
+                             `<article> ${ALBUMES[4].album} </article>`
+                             `<article> ${ALBUMES[5].album} </article>`
+                             `<article> ${ALBUMES[6].album} </article>`
+                             `<article> ${ALBUMES[7].album} </article>`
+                             `<article> ${ALBUMES[8].album} </article>`
+                             `<article> ${ALBUMES[9].album} </article>`
+                             `<article> ${ALBUMES[10].album} </article>`
+                             `<article> ${ALBUMES[11].album} </article>`
+                             `<article> ${ALBUMES[12].album} </article>`
+                             `<article> ${ALBUMES[13].album} </article>`
+                             `<article> ${ALBUMES[14].album} </article>`
+                             `<article> ${ALBUMES[15].album} </article>`*/
+
+
+
+                            
