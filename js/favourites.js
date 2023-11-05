01a4626c76e6be5af8f674d0fb570bb06a3da8b1
agussdangelo <agustinagustindangelo@hotmail.com>
2023-11-04 21:24:17 -0300

    se cambio el css de playing y mejoro el js de favoritos

--- js/favourites.js
+++ js/favourites.js
@@ -1,69 +1,74 @@
 document.addEventListener("DOMContentLoaded", function() {
     const profileTitle = document.querySelector('.profile__title');
     const profileLinkSession = document.querySelector('.profile__link-session');
-    
-    // Verificar si el usuario está registrado
-    const loggedInUsername = localStorage.getItem('loggedInUsername');
 
-    if (loggedInUsername) {
+    // Cargar los usuarios desde el localStorage
+    const users = JSON.parse(localStorage.getItem('users'));
+
+    // Verificar si el usuario está registrado
+    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+
+    if (loggedInUser) {
         // Si el usuario está registrado, muestra el nombre de usuario y la opción de cerrar sesión
-        profileTitle.textContent = loggedInUsername;
-        
+        profileTitle.textContent = loggedInUser.username;
+
         // Agregar un evento al enlace de "Cerrar sesión" para marcar que el usuario cerró sesión
         profileLinkSession.addEventListener('click', function() {
-            localStorage.removeItem('loggedInUsername'); // Marcar que el usuario cerró sesión
+            localStorage.removeItem('loggedInUser'); // Marcar que el usuario cerró sesión
             profileTitle.textContent = 'Nombre de usuario'; // Restablecer el nombre de usuario en la interfaz
         });
     } else {
         // Si el usuario no está registrado, muestra un valor predeterminado o un mensaje
         profileTitle.textContent = 'Nombre de usuario'; // Puedes personalizar el mensaje para invitados
     }
-});
 
-// Función para cargar los álbumes favoritos desde el localStorage
-function cargarAlbumsFavoritos() {
-    const favoritos = localStorage.getItem("favoritos");
-    return favoritos ? JSON.parse(favoritos) : [];
-}
+    // Seleccionar todas las estrellas
+    const stars = document.querySelectorAll(".songs__star");
 
-// Función para guardar los álbumes favoritos en el localStorage
-function guardarAlbumsFavoritos(favoritos) {
-    localStorage.setItem("favoritos", JSON.stringify(favoritos));
-}
+    // Obtener el índice del usuario actual en el array de usuarios
+    const userIndex = users.findIndex((user) => user.username === loggedInUser.username);
 
-// Seleccionar todos los elementos con la clase 'songs__article'
-const songsArticles = document.querySelectorAll(".songs__article");
+    // Cargar los álbumes favoritos del usuario actual desde el objeto del usuario
+    const favoritos = loggedInUser.albumsFavoritos || [];
 
-// Cargar los álbumes favoritos existentes desde el localStorage
-const favoritos = cargarAlbumsFavoritos();
+    // Iterar a través de las estrellas
+    stars.forEach((star, index) => {
+        star.addEventListener("click", () => {
+            // Verificar si el álbum ya es un favorito
+            const isFavorite = star.classList.contains("selected");
+            const albumName = document.querySelectorAll(".songs__image")[index].getAttribute("alt");
 
-// Iterar a través de los elementos
-songsArticles.forEach((article) => {
-    // Encontrar la estrella dentro de este artículo
-    const star = article.querySelector(".songs__star");
-
-    // Obtener el nombre del álbum
-    const albumName = article.querySelector(".songs__image").getAttribute("src");
-
-    // Agregar un manejador de eventos al hacer clic en la estrella
-    star.addEventListener("click", () => {
-        const isFavorite = star.classList.contains("selected")
-        if (isFavorite) {
-            // Si ya es un favorito, eliminarlo de la lista de favoritos y actualizar el estilo
-            const indexToRemove = favoritos.indexOf(albumName);
-            if (indexToRemove !== -1) {
-                favoritos.splice(indexToRemove, 1);
-                guardarAlbumsFavoritos(favoritos);
-
+            if (isFavorite) {
+                // Si ya es un favorito, quitarlo de la lista de favoritos y actualizar el estilo
+                favoritos.splice(favoritos.indexOf(albumName), 1);
+                star.classList.remove("selected");
+            } else {
+                // Si no es un favorito, agregarlo a la lista de favoritos y actualizar el estilo
+                favoritos.push(albumName);
+                star.classList.add("selected");
             }
-            star.classList.remove("selected");
-        } else {
-            // Si no es un favorito, agregarlo a la lista de favoritos y actualizar el estilo
-            favoritos.push(albumName);
-            guardarAlbumsFavoritos(favoritos);
-            star.classList.add("selected");
-            
-        }
+
+            // Actualizar la lista de álbumes favoritos del usuario en el objeto del usuario
+            loggedInUser.albumsFavoritos = favoritos;
+
+            // Actualizar el objeto del usuario en el localStorage
+            localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser));
+
+            // Actualizar el array de usuarios en el localStorage
+            users[userIndex] = loggedInUser;
+            localStorage.setItem('users', JSON.stringify(users));
+        });
+    });
+
+    // Marcar las estrellas según los álbumes favoritos del usuario
+    favoritos.forEach((albumName) => {
+        // Aquí, puedes utilizar el albumName para identificar y marcar las estrellas de los álbumes favoritos
+        // Puedes comparar el valor de albumName con el atributo alt de las imágenes para determinar cuáles deben estar marcadas.
+        document.querySelectorAll(".songs__image").forEach((image, index) => {
+            if (image.getAttribute("alt") === albumName) {
+                stars[index].classList.add("selected");
+            }
+        });
     });
 });
 
@@ -107,54 +112,3 @@ cancion6.addEventListener("click", function() {
     songAside.src= '../img/Idontwanttomissathing cancion 6.jpg';
     texto.textContent = "La canción I Don't Want to Miss a Thing de Aerosmith es una declaración de amor y devoción, expresando admiración por alguien tan envuelto en ellos que nunca quiere separarse de su lado. La letra habla del deseo de permanecer en el momento para siempre y nunca dejar ir su amor."
 })
-
-
-// Al hacer click en las estrellas, el álbum se debe agregar/retirar como álbum favorito.
-
-// const songsArticlesStars = document.getElementById('songs__content');
-
-// songsArticlesStars.forEach((article) => {
-//     const estrella = article.getElementsById('estrella');
-//     let colorAlternado = false; // Variable para alternar el color\
-
-//     estrella.addEventListener('click', () => {
-//         if (colorAlternado) {
-//             estrella.classList.remove('azul');
-//             estrella.classList.add('amarilla');
-//         } else {
-//             estrella.classList.remove('amarilla');
-//             estrella.classList.add('azul');
-//         }
-//         colorAlternado = !colorAlternado;
-//     });
-// })
-
-
-// // agregar albumes a favoritos al hacer click en la estrella
-
-// const estrella = document.querySelectorAll(".favAlbumSonando")
-
-// estrella.forEach(function(estrella){
-//     estrella.addEventListener("click",function () {
-
-//         estrella.classList.toggle("fondo")
-
-//         for (const usuario of usuariosRegistrados) {
-
-//             if (usuario.enLinea==true) {
-//                 const album=this.alt
-//                 let albumFav=usuario.albumsFav
-//                 const contieneAlbum=albumFav.some((albumbuscado)=>albumbuscado===album)
-
-//                 if (contieneAlbum) {
-//                     const indiceAlbum=albumFav.indexOf(album)
-//                     albumFav.splice(indiceAlbum,1)
-//                 }else{
-//                     albumFav.push(album);
-//                 }
-//                 }
-//             }
-//                 localStorage.setItem("usuarios",JSON.stringify(usuariosRegistrados));
-//     })
-// })
-
