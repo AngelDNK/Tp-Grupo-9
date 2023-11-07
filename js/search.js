4b3480c502c37150924cac36ee7a08662ba69093
Angel Leyes <angelleyesdk@gmail.com>
2023-11-07 18:54:59 -0300

    SE ARREGLO EL BUSCADOR
    SE ARREGLO EL BUSCADOR

--- js/search.js
+++ js/search.js
@@ -1,44 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     const profileTitle = document.querySelector('.profile__title');
-    const profileLinkSession = document.querySelector('.profile__link-session');
+    const songsSection = document.querySelector(".songs__section");
+
+    // Cargar los usuarios desde el localStorage
+    const users = JSON.parse(localStorage.getItem('users'));
 
     // Verificar si el usuario está registrado
-    const loggedInUsername = localStorage.getItem('loggedInUsername');
+    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
 
-    if (loggedInUsername) {
-        // Si el usuario está registrado, muestra el nombre de usuario y la opción de cerrar sesión
-        profileTitle.textContent = loggedInUsername;
-
-        // Agregar un evento al enlace de "Cerrar sesión" para marcar que el usuario cerró sesión
-        profileLinkSession.addEventListener('click', function () {
-            localStorage.removeItem('loggedInUsername'); // Marcar que el usuario cerró sesión
-            profileTitle.textContent = 'NombreDeUsuario'; // Restablecer el nombre de usuario en la interfaz
-        });
+    if (loggedInUser) {
+        // Si el usuario está registrado, muestra el nombre de usuario
+        profileTitle.textContent = loggedInUser.username;
     } else {
         // Si el usuario no está registrado, muestra un valor predeterminado o un mensaje
-        profileTitle.textContent = 'NombreDeUsuario'; // Puedes personalizar el mensaje para invitados
+        profileTitle.textContent = 'Nombre de usuario'; // Puedes personalizar el mensaje para invitados
     }
 });
-
-/*document.addEventListener('DOMContentLoaded', function () {
-    const search__Input = document.getElementById('search');
-    const songs__section = document.querySelector('.songs__section');
-
-    search__Input.addEventListener('input', function () {
-        const search = search__Input.value.toLowerCase();
-
-        const articles = songs__section.querySelectorAll('.songs__article');
-        articles.forEach(function (article) {
-            const image = article.querySelector('.songs__image');
-            const description = article.querySelector('song__description');
-            const title = description.textContent.toLowerCase();
-
-        });
-    });
-});*/
-
-
-const ALBUM1 = { album: document.querySelector('#cancerbero') }
+/*const ALBUM1 = { album: document.querySelector('#cancerbero') }
 const ALBUM2 = { album: document.querySelector('#salandolasheridas') }
 const ALBUM3 = { album: document.querySelector('#ciro') }
 const ALBUM4 = { album: document.querySelector('#wos') }
@@ -57,12 +35,10 @@ const ALBUM16 = { album: document.querySelector('#somosmagic') }
 const ALBUMES = [ALBUM1, ALBUM2, ALBUM3, ALBUM4, ALBUM5, ALBUM6, ALBUM7, ALBUM8, ALBUM9, ALBUM10, ALBUM12, ALBUM13, ALBUM14, ALBUM15, ALBUM16];
 
 const NODO_ALBUM_BUSCAR = document.querySelector('#search');
-const songsSection = document.querySelector("#songs_section");
 
 NODO_ALBUM_BUSCAR.addEventListener("input", function (e) {
     const PALABRA_A_BUSCAR = NODO_ALBUM_BUSCAR.value.toLowerCase();
 
-   
     songsSection.innerHTML = "";
 
     ALBUMES.forEach(albumObj => {
@@ -75,29 +51,63 @@ NODO_ALBUM_BUSCAR.addEventListener("input", function (e) {
                 const images = clonedAlbum.querySelectorAll('img');
                 images.forEach(img => {
                     img.style.width = '450px'; 
-                    img.style.height = '450px'; 
+                    img.style.height = '450px';
+                    
                 });
                 songsSection.appendChild(clonedAlbum);
             }
         }
     });
 });
+});
+
+window.addEventListener('resize', () => {
+    const img = document.querySelectorAll('.img');
+    if (window.innerWidth < 997) {
+        // Aplicar estilos para pantallas más pequeñas
+        img.forEach(img => {
+            article.style.width = '5%';
+            article.style.height = '5%';
+            article.style.margin = '100px';
+        });
+    } else {
+        // Restaurar los estilos para pantallas más grandes
+        img.forEach(img => {
+            img.style.margin = '100px'; // Ajusta el ancho según tus necesidades
+        });
+    }
+});*/
 
 
+const search = document.querySelector("#search");
+const songs_section = document.querySelector("#songs_section");
+const albums_image = document.querySelectorAll(".songs__image");
+const ALBUMS = Array.from(albums_image)
 
-    /*`<article> ${ALBUMES[0].album} </article>`
-                             `<article> ${ALBUMES[1].album} </article>`
-                             `<article> ${ALBUMES[2].album} </article>`
-                             `<article> ${ALBUMES[3].album} </article>`
-                             `<article> ${ALBUMES[4].album} </article>`
-                             `<article> ${ALBUMES[5].album} </article>`
-                             `<article> ${ALBUMES[6].album} </article>`
-                             `<article> ${ALBUMES[7].album} </article>`
-                             `<article> ${ALBUMES[8].album} </article>`
-                             `<article> ${ALBUMES[9].album} </article>`
-                             `<article> ${ALBUMES[10].album} </article>`
-                             `<article> ${ALBUMES[11].album} </article>`
-                             `<article> ${ALBUMES[12].album} </article>`
-                             `<article> ${ALBUMES[13].album} </article>`
-                             `<article> ${ALBUMES[14].album} </article>`
-                             `<article> ${ALBUMES[15].album} </article>`*/
+function resetALBUMS() {
+
+    if (songs_section) {
+    
+        while (songs_section.firstChild) {
+            songs_section.removeChild(songs_section.firstChild)
+        
+        }
+    }
+}
+search.addEventListener('keyup', function (e) {
+    const PALABRA_INGRESADA =search.value
+ 
+    resetALBUMS()
+    
+    const ALBUM_SEARCH=ALBUMS.filter(img=>img.getAttribute("data-alt").includes(PALABRA_INGRESADA))
+
+
+  ALBUM_SEARCH.forEach(img => {
+    img.style.width = '450px'; 
+    img.style.height = '450px'; 
+
+    songs_section.appendChild(img)
+   
+});
+});
+
