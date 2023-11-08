b0ed618681645b488bf117382a065fa3fc524b47
Juan Ignacio Ronquillo <ronquillojuanignacio@gmail.com>
2023-11-08 00:36:52 -0300

    Unificando profile, y search

--- js/search.js
+++ js/search.js
@@ -16,48 +16,22 @@ document.addEventListener("DOMContentLoaded", function () {
         profileTitle.textContent = 'Nombre de usuario'; // Puedes personalizar el mensaje para invitados
     }
 
-    const ALBUM1 = { album: document.querySelector('#cancerbero') }
-const ALBUM2 = { album: document.querySelector('#salandolasheridas') }
-const ALBUM3 = { album: document.querySelector('#ciro') }
-const ALBUM4 = { album: document.querySelector('#wos') }
-const ALBUM5 = { album: document.querySelector('#tupac') }
-const ALBUM6 = { album: document.querySelector('#idontwanttomissathing') }
-const ALBUM7 = { album: document.querySelector('#columbarecords') }
-const ALBUM8 = { album: document.querySelector('#kirby') }
-const ALBUM9 = { album: document.querySelector('#roadsong') }
-const ALBUM10 = { album: document.querySelector('#abba') }
-const ALBUM12 = { album: document.querySelector('#random') }
-const ALBUM13 = { album: document.querySelector('#disc') }
-const ALBUM14 = { album: document.querySelector('#piromania') }
-const ALBUM15 = { album: document.querySelector('#harrystyles') }
-const ALBUM16 = { album: document.querySelector('#somosmagic') }
-
-const ALBUMES = [ALBUM1, ALBUM2, ALBUM3, ALBUM4, ALBUM5, ALBUM6, ALBUM7, ALBUM8, ALBUM9, ALBUM10, ALBUM12, ALBUM13, ALBUM14, ALBUM15, ALBUM16];
-
-const NODO_ALBUM_BUSCAR = document.querySelector('#search');
-
-NODO_ALBUM_BUSCAR.addEventListener("input", function (e) {
-    const PALABRA_A_BUSCAR = NODO_ALBUM_BUSCAR.value.toLowerCase();
-
-    songsSection.innerHTML = "";
-
-    ALBUMES.forEach(albumObj => {
-        if (albumObj && albumObj.album) {
-            const albumNombre = albumObj.album.id.toLowerCase();
-
-            if (albumNombre.includes(PALABRA_A_BUSCAR)) {
-                
-                const clonedAlbum = albumObj.album.closest('article').cloneNode(true);
-                const images = clonedAlbum.querySelectorAll('img');
-                images.forEach(img => {
-                    img.style.width = '450px'; 
-                    img.style.height = '450px'; 
-                });
-                songsSection.appendChild(clonedAlbum);
+    
+        const searchInput = document.getElementById("search");
+        const songs = document.querySelectorAll(".songs__article");
+      
+        searchInput.addEventListener("input", function () {
+          const searchTerm = searchInput.value.toLowerCase();
+      
+          songs.forEach((song) => {
+            const songName = song.querySelector("img").getAttribute("data-alt").toLowerCase();
+            if (songName.includes(searchTerm)) {
+              song.style.display = "block";
+            } else {
+              song.style.display = "none";
             }
-        }
-    });
+          });
+        });
+      
 });
-});
-
 
